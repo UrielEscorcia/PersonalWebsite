ff5b181fa3f698529f26f9b59234a1c4763eb5fe
Uriel Escorcia <uris_ec@hotmail.com>
2015-02-26 02:00:27 -0600

    Add images to portfolio & contact form send email

--- send.php
+++ send.php
@@ -4,24 +4,20 @@ if ($_POST) {
 	$name = $_POST['name'];
 	$email = $_POST['email'];
 	$message = $_POST['message'];
-	$to_email = "uris.ec10@gmail.com";
+	$to_email = "arnoldgerardo@icloud.com";
 
-	$subject = 'Mensaje  prueba';
-	$message = 'FROM: '.$name.' Email: '.$email.'Message: '.$message;
-	$headers = 'From: uris_ec@hotmail.com' . "\r\n";
+	$subject = 'Contacto Arnoldo';
+	$message = 'FROM: '.$name.' Email: '.$email."\r\n".' Message: '.$message;
+	$headers = 'From: uris.ec10@gmail.com' . "\r\n";
 
 	if (filter_var($email, FILTER_VALIDATE_EMAIL)) { // this line checks that we have a valid email address
-		mail($to_email, $subject, $message, $headers); //This method sends the mail.
-		echo "mensaje enviado";
-	}else{
-		 echo "no se pudo";
-	}
+		if (mail($to_email, $subject, $message, $headers))  //This method sends the mail.
+			echo json_encode(array("type"=>"message", "text" => "Datos Enviados, Gracias."));	
+		else
+			echo json_encode(array("type"=>"error", "text" => "Error al mandar datos! Por favor intentalo más tarde."));	
+	}else
+		echo json_encode(array("type"=>"error", "text" => "Error al mandar datos! Por favor verifica tu Email."));
 }
 	 
 ?>
-
-
-
-
-
 
